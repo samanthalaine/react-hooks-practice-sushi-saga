0241fe8a5a5d57170eca8bf45d5e724379bedaee
Samantha Laine <samanthalaine8@gmail.com>
2021-08-18 18:34:14 -0400

    finished first 2 deliverables

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
 function App() {
+
+  const [allSushi, setAllSushi] = useState([])
+
+useEffect(()=>{
+  fetch(API)
+  .then(r=>r.json())
+  .then(data=>setAllSushi(data))
+
+},[])
+
+const displaySushi = allSushi.slice(0,4)
+
   return (
     <div className="app">
-      <SushiContainer />
-      <Table />
+      <SushiContainer allSushi={displaySushi}/>
     </div>
   );
 }
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,10 +1,16 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from './Sushi'
 
 function SushiContainer(props) {
+
+  const {allSushi} = props
+
+  const displaySushi = allSushi.map(sushi=> <Sushi key={sushi.id} sushi={sushi}/>)
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
+      {displaySushi}
       <MoreButton />
     </div>
   );
